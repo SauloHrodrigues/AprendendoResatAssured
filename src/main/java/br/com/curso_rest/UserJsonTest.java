c0339d12f2fa421cdae6634d5b6fb868f023ba33
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2023-08-01 16:48:44 -0300

    Validando JSON com listas

--- src/main/java/br/com/curso_rest/UserJsonTest.java
+++ src/main/java/br/com/curso_rest/UserJsonTest.java
@@ -61,5 +61,22 @@ public class UserJsonTest {
 			.body("endereco.rua", Matchers.is("Rua dos bobos"))
 		;
 	}
+	@Test
+	public void jsonLista() {
+		RestAssured
+		.given()
+		.when()
+		.get("https://restapi.wcaquino.me/users/3")
+		.then()
+		.statusCode(200)
+		.body("id", Matchers.is(3))
+		.body("name", Matchers.containsString("Ana"))
+		.body("filhos", Matchers.hasSize(2))
+		.body("filhos[0].name", Matchers.is("Zezinho"))
+		.body("filhos[1].name", Matchers.is("Luizinho"))
+		.body("filhos.name", Matchers.hasItem("Luizinho"))
+		.body("filhos.name", Matchers.hasItems("Luizinho", "Zezinho"))
+		;
+	}
 	
 }
