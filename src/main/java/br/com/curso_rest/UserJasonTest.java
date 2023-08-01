241ac19aacc18ebe0b51458dd4d232928c676725
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2023-08-01 16:00:01 -0300

    Validando JSON primeiro nível II

--- src/main/java/br/com/curso_rest/UserJasonTest.java
+++ src/main/java/br/com/curso_rest/UserJasonTest.java
@@ -1,9 +1,16 @@
 package br.com.curso_rest;
 
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+
+
 
 public class UserJasonTest {
 
@@ -19,5 +26,25 @@ public class UserJasonTest {
 			.body("name", Matchers.containsString("Silva"))
 			.body("age", Matchers.greaterThan(18))
 		;
+	}
+	
+	
+	@SuppressWarnings("removal")
+	@Test
+	public void jasonPrimeiroNivel_II() {
+		Response response = RestAssured.request(Method.GET,"https://restapi.wcaquino.me/users/1");
+		
+		// path
+		Assert.assertEquals(new Integer(1), response.path("id"));
+		Assert.assertEquals(new Integer(1), response.path("%s","id"));
+		
+//		jsonpath	
+		JsonPath jsonPath= new JsonPath(response.asString());
+		Assert.assertEquals(1, jsonPath.getInt("id"));
+		
+//		from
+		int id = JsonPath.from(response.asString()).getInt("id");
+		Assert.assertEquals(1, id);
+		
 	}
 }
